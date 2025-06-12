19eca88b83ebcbac6e970ad34daf5bddc3b2d6bd
xuhaitong <xuhaitong23@mails.ucas.ac.cn>
2025-06-12 21:12:31 +0800

    fix:json parse error

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import os
 from tqdm import tqdm
 from typing import List, Dict, Tuple, Any, Optional
 from concurrent.futures import ThreadPoolExecutor, as_completed
+import re
 
 def load_alpaca_data(file_path:str) -> List[Dict[str, Any]]:
     with open(file_path, 'r', encoding='utf-8') as f:
@@ -28,7 +29,35 @@ def save_alpaca_data(data:List[Dict[str, Any]], store_dir:str, file_name:str):
         json.dump(data, f, ensure_ascii=False, indent=2)
     print(f"Data saved to {file_path}")
 
+def process_answer_content_to_json(answer_content: str) -> Dict[str, Any]:
+    try:
+        return json.loads(answer_content)
+    except json.JSONDecodeError:
+        pass
+    try:
+        # 1. clean // comment
+        lines = answer_content.splitlines()
+        no_comment_lines = [re.sub(r'//.*', '', line) for line in lines]
+        text = '\n'.join(no_comment_lines)
 
+        # 2. fix key-value "key: "value" -> "key": "value"
+        # 
+        text = re.sub(r'"([^"]+?):\s*"', r'"\1": "', text)
+
+        # 3. add "" 
+        text = re.sub(
+            r'(?<!")\b([a-zA-Z0-9_\u4e00-\u9fa5]+)\b(?=\s*:)', r'"\1"', text
+        )
+
+        # 4. remove last item ","
+        text = re.sub(r',(\s*[}\]])', r'\1', text)
+
+        # 5. parse json
+        return json.loads(text)
+
+    except json.JSONDecodeError as e:
+        raise ValueError(f"JSON parse error：{e}")
+    
 
 ## process single request to inference server
 def process_prompt_to_client_single(all_prompt_list: List[Dict[str, Any]], inference_client: Any, inference_logger: Any, **kwargs) -> List[Dict[str, Any]]:
@@ -46,8 +75,8 @@ def process_prompt_to_client_single(all_prompt_list: List[Dict[str, Any]], infer
                 _, answer_content = inference_client.get_response(prompt, **kwargs)
                 # print(f"response: {_}")
                 # print(f"answer_content: {answer_content}")
-                answer_json = json.loads(str(answer_content))
-
+                answer_json = process_answer_content_to_json(str(answer_content))
+                
                 inference_logger.info(f"Processed num {index+1} prompt")
                 for key, value in answer_json.items():
                     combine_response_dict[key] = value
@@ -96,7 +125,7 @@ def process_prompt_to_client(all_prompt_list: List[Dict[str, Any]], inference_cl
         for prompt in prompt_list:
             try:
                 _, answer_content = inference_client.get_response(prompt, **kwargs)
-                answer_json = json.loads(str(answer_content))
+                answer_json = process_answer_content_to_json(str(answer_content))
 
                 for key, value in answer_json.items():
                     combine_response_dict[key] = value
